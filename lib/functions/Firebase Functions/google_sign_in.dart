2aa94a5d7f725e4c193d740235febfa75c0723cd
Aryan_2002 <aryansood12@gmail.com>
2024-03-15 01:13:31 +0530

    Revert "Revert "Added Email Login""
    This reverts commit de34a7b330a86f0e9e03933fff618c62e049bb61.

---
+++ lib/functions/Firebase Functions/google_sign_in.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
+Future<void> signInWithGoogle(BuildContext context) async {
+  try {
+    final GoogleSignIn googleSignIn = GoogleSignIn();
+    final GoogleSignInAccount? googleSignInAccount =
+        await googleSignIn.signIn();
+    if (googleSignInAccount != null) {
+      final GoogleSignInAuthentication googleAuth =
+          await googleSignInAccount.authentication;
+      final AuthCredential authCredential = GoogleAuthProvider.credential(
+        accessToken: googleAuth.accessToken,
+        idToken: googleAuth.idToken,
+      );
+    }
+  } catch (error) {
+    print('{Error Signing In: $error}');
+  }
+}
