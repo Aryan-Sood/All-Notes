4f7896cafa4a31f1ee7e6fcd4bde3111ae39f49a
Aryan_2002 <aryansood12@gmail.com>
2024-03-17 15:01:40 +0530

    Removed Hive for now & added color picker

---
+++ lib/models/note_structure.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class NoteStructure {
+  late Color color;
+  late String title;
+
+  NoteStructure({required this.color, required this.title});
+}
--- lib/widgets/add_note_sheet.dart
+++ lib/widgets/add_note_sheet.dart
@@ -1,80 +1,105 @@
-import 'package:all_notes/models/note.dart';
+import 'package:all_notes/models/note_structure.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_colorpicker/flutter_colorpicker.dart';
+
+Color currentColor = Colors.green;
+
+void setColor(Color color) {
+  currentColor = color;
+}
+
+// String colorToString(Color color) {
+//   return '#${color.value.toRadixString(16).substring(2)}';
+// }
+
+// Color stringToColor(String color) {
+//   return colorFromHex(color) ?? Colors.green;
+// }
 
 void AddNoteSheet(BuildContext context, Function(NoteStructure) updateData) {
   final TextEditingController _titleController = TextEditingController();
   showModalBottomSheet(
-      isScrollControlled: false,
+      isScrollControlled: true,
       context: context,
       builder: (BuildContext context) {
-        return Padding(
-          padding: EdgeInsets.only(
-            left: 20,
-            right: 20,
-          ),
+        return SingleChildScrollView(
           child: Padding(
             padding: EdgeInsets.only(
-              top: 40,
+              left: 20,
+              right: 20,
             ),
-            child: Container(
-              height: MediaQuery.of(context).size.height - 50,
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.start,
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  Text(
-                    "New Note",
-                    style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
-                  ),
-                  TextField(
-                    keyboardType: TextInputType.emailAddress,
-                    textAlign: TextAlign.center,
-                    textAlignVertical: TextAlignVertical.center,
-                    controller: _titleController,
-                    decoration: InputDecoration(
-                        labelText: "Title", alignLabelWithHint: true),
-                  ),
-                  const SizedBox(
-                    height: 40,
-                  ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    children: [
-                      ElevatedButton(
-                        onPressed: () {
-                          Navigator.of(context).pop();
-                        },
-                        child: Text(
-                          'Cancel',
-                          style: TextStyle(
-                            fontSize: 16,
-                            fontWeight: FontWeight.bold,
-                          ),
-                        ),
-                      ),
-                      ElevatedButton(
-                        onPressed: () {
-                          if (_titleController.text.isNotEmpty) {
-                            updateData(
-                              NoteStructure(
-                                  Colors.green, _titleController.text),
-                            );
-
+            child: Padding(
+              padding: EdgeInsets.only(
+                top: 40,
+              ),
+              child: Container(
+                height: MediaQuery.of(context).size.height - 50,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Text(
+                      "New Note",
+                      style:
+                          TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
+                    ),
+                    TextField(
+                      keyboardType: TextInputType.emailAddress,
+                      textAlign: TextAlign.center,
+                      textAlignVertical: TextAlignVertical.center,
+                      controller: _titleController,
+                      decoration: InputDecoration(
+                          labelText: "Title", alignLabelWithHint: true),
+                    ),
+                    const SizedBox(
+                      height: 40,
+                    ),
+                    ColorPicker(
+                      pickerColor: currentColor,
+                      onColorChanged: setColor,
+                      displayThumbColor: false,
+                      hexInputBar: false,
+                      paletteType: PaletteType.hueWheel,
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        ElevatedButton(
+                          onPressed: () {
                             Navigator.of(context).pop();
-                          }
-                        },
-                        child: Text(
-                          'Done',
-                          style: TextStyle(
-                            fontSize: 16,
-                            fontWeight: FontWeight.bold,
+                          },
+                          child: Text(
+                            'Cancel',
+                            style: TextStyle(
+                              fontSize: 16,
+                              fontWeight: FontWeight.bold,
+                            ),
                           ),
                         ),
-                      ),
-                    ],
-                  )
-                ],
+                        ElevatedButton(
+                          onPressed: () {
+                            if (_titleController.text.isNotEmpty) {
+                              updateData(
+                                NoteStructure(
+                                    color: currentColor,
+                                    title: _titleController.text),
+                              );
+                              Navigator.of(context).pop();
+                            }
+                          },
+                          child: Text(
+                            'Done',
+                            style: TextStyle(
+                              fontSize: 16,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        ),
+                      ],
+                    )
+                  ],
+                ),
               ),
             ),
           ),
